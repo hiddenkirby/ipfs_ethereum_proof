3e4ef0ce7be90df2d4ddb0ec283ef2fc595fbae5
hiddenkirby <hiddenkirby@gmail.com>
2018-10-14 19:00:39 -0400

    still having some issues figure out how to manage gas

---
+++ src/storehash.js
@@ -0,0 +1,35 @@
+import web3 from './web3';
+//Your contract address
+const address = '0x585dbf66c2498ab257dad138f19cfaad93b30710';
+//Your contract ABI
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "setHash",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHash",
+		"outputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+export default new web3.eth.Contract(abi, address);
